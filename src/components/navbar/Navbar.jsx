326648a91b737410eb5c53d9da585890377260ef
Harshthakur23 <harshbhadouriya1@gmail.com>
2024-08-15 02:10:39 +0530

    Commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import "./navbar.scss";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <motion.span initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>
+          Harsh Bhadouriya
+        </motion.span>
+        <div className="social">
+          <a href="#"><img src="/x.jpg" alt="Social Media 1" /></a>
+          <a href="#"><img src="/linkdn.png" alt="LinkedIn" /></a>
+          <a href="#"><img src="/gihub.png" alt="GitHub" /></a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

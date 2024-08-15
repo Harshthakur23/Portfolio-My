9ef48d0d176814143913b2639a6e59e9451371e8
Harshthakur23 <harshbhadouriya1@gmail.com>
2024-08-15 20:13:39 +0530

    Update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,15 @@
+import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <div className="navbar">
+      <Sidebar/>
       <div className="wrapper">
-        <motion.span initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>
+        {/* <motion.span initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}> */}
           Harsh Bhadouriya
-        </motion.span>
+        {/* </motion.span> */}
         <div className="social">
           <a href="#"><img src="/x.jpg" alt="Social Media 1" /></a>
           <a href="#"><img src="/linkdn.png" alt="LinkedIn" /></a>
